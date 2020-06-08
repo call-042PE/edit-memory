d12a552e3f1a34238764a7f549dae16cd044d44e
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-06-08 20:47:27 +0200

    Update AssaultCube.cpp

--- Example/AssaultCube.cpp
+++ Example/AssaultCube.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	DWORD procId = GetProcId(L"ac_client.exe");
 	//getting procId.
-	
+
 	DWORD moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
 	//getting ModuleBase.
 
@@ -13,18 +13,18 @@ int main()
 	//Opens an existing local process object. https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
 
 	DWORD localPlayer = moduleBase + 0x10F4F4;
-	
+
 	std::vector<unsigned int> ammoOffsets = { 0x150 };
 
 	uintptr_t ammoAddr = FindDynamicAddr(handle, localPlayer, ammoOffsets);
 	//finding dynamic address for reading/writing/patching it.
 
-	editmemory::WriteMem(handle, (BYTE*)ammoAddr, (BYTE*)1337);
+	editmemory::WriteMem<int>(handle, (LPVOID)ammoAddr, 1337);
 	//writing to the ammoAddr 1337 value.
 
 	while (true)
 	{
-		int currentAmmo = editmemory::ReadMem<int>(handle, (BYTE*)ammoAddr);
+		int currentAmmo = editmemory::ReadMem<int>(handle, (LPVOID)ammoAddr);
 		//reading value in ammoAddr and stock it in currentAmmo integer.
 		std::cout << currentAmmo << std::endl;
 		//output currentAmmo integer value.
